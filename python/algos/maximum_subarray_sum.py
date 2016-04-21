d6cd46dc9a4937a7201ffbc27fe9ae92e5d26ba7
patrick <patrick@patrickdeMacBook-Pro.local>
2016-04-21 10:50:48 +0800

    update maximium subarray sum

---
+++ python/algos/maximum_subarray_sum.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+def maximum_subarray_sum(array=[]):
+    max_ending_here = max_so_far = array[0]
+    for item in array[1:]:
+        max_ending_here = max(0, max_ending_here + item)
+        max_so_far = max(max_so_far, max_ending_here)
+    return max_so_far
+
+
+print(maximum_subarray_sum([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
